e8d94d2a14b361bcb86e2507ca5c814fe6506879
Farhan <mansurifarhanfm@gmail.com>
2022-11-23 17:22:39 +0530

    hu

---
+++ components/LoginModal.jsx
@@ -0,0 +1,67 @@
+import { Button, Checkbox, Input, Modal, Row, Text } from "@nextui-org/react";
+import React from "react";
+
+export default function LoginModal() {
+  const [visible, setVisible] = React.useState(false);
+  const handler = () => setVisible(true);
+
+  const closeHandler = () => {
+    setVisible(false);
+    console.log("closed");
+  };
+
+  return (
+    <div>
+      <Button auto shadow onClick={handler}>
+        Login
+      </Button>
+      <Modal
+        closeButton
+        aria-labelledby="modal-title"
+        open={visible}
+        onClose={closeHandler}
+      >
+        <Modal.Header>
+          <Text id="modal-title" size={18}>
+            Welcome to {' '}
+            <Text b size={18}>
+              ShoppingWebsite
+            </Text>
+          </Text>
+        </Modal.Header>
+        <Modal.Body>
+          <Input
+            clearable
+            bordered
+            fullWidth
+            color="primary"
+            size="lg"
+            placeholder="Email"
+          />
+          <Input
+            clearable
+            bordered
+            fullWidth
+            color="primary"
+            size="lg"
+            placeholder="Password"
+          />
+          <Row justify="space-between">
+            <Checkbox>
+              <Text size={14}>Remember me</Text>
+            </Checkbox>
+            <Text size={14}>Forgot password?</Text>
+          </Row>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button auto flat color="error" onClick={closeHandler}>
+            Close
+          </Button>
+          <Button auto onClick={closeHandler}>
+            Sign in
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+}
---
+++ pages/login.jsx
@@ -0,0 +1,53 @@
+import { Input, Spacer, Text, useInput } from '@nextui-org/react';
+import React from 'react';
+export default function login() {
+  const validateEmail = (value) => {
+    return value.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$/i);
+  };
+  const { value, reset, bindings } = useInput('');
+  const helper = React.useMemo(() => {
+    if (!value)
+      return {
+        text: '',
+        color: '',
+      };
+    const isValid = validateEmail(value);
+    return {
+      text: isValid ? 'Correct email' : 'Enter a valid email',
+      color: isValid ? 'success' : 'error',
+    };
+  }, [value]);
+  return (
+    <div className="w-10/12  mx-auto">
+      <div className="mx-auto w-fit  py-5">
+       
+        <Text
+          h1
+          size={50}
+          css={{
+            textGradient: '45deg, $yellow600 -20%, $red600 100%',
+          }}
+          weight="bold"
+        >
+        Login
+        </Text>
+        <Input
+          width="300px"
+          clearable
+          shadow={true}
+          type="email"
+          label="Email"
+          status={helper.color}
+          color={helper.color}
+          helperColor={helper.color}
+          helperText={helper.text}
+          onClearClick={reset}
+          placeholder="Enter your e-mail address "
+           
+        />
+        <Spacer y={1.0} />
+        <Input.Password width="300px" clearable helperText="Insecure password" type="password" label="Password" placeholder="Enter your password " />
+      </div>
+    </div>
+  );
+}
