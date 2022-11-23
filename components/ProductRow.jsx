19a183c5100264bc1a6a7daf8c5e33befd74b550
Farhan <mansurifarhanfm@gmail.com>
2022-11-23 13:01:08 +0530

    vv

--- components/ProductRow.jsx
+++ components/ProductRow.jsx
@@ -7,7 +7,7 @@ export default function ProductRow({data ,title}) {
 
       <div className="  mx-auto gap-3 grid w-11/12 lg:w-10/12 grid-cols-2 lg:grid-cols-3">
         {data.map((e) => (
-          <Product e={e} />
+          <Product e={e} key={e.id} />
         ))}
       </div>
     </>
