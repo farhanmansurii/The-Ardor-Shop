b26c3c80b55ceb77efe4dbc9b62fe1729329f34c
Farhan <mansurifarhanfm@gmail.com>
2022-11-24 18:01:57 +0530

    g

--- components/Product.jsx
+++ components/Product.jsx
@@ -9,12 +9,12 @@ export default function Product({ e }) {
     dispatch(add(product));
   };
   return (
-    <div key={e.id} className="flex flex-col py-2 font-semibold mx-auto font-whyte hover:scale-[98%] duration-200">
+    <div key={e.id} className="flex flex-col py-2 space-y-4 font-semibold mx-auto font-whyte hover:scale-[98%] duration-200">
       <Link href={`/${e.id}`}>
         <Image width={300} height={200} src={e.images[0]} alt={e.title} className="  " />
-        <div className=" text-sm lg:text-xl flex px-1 pt-2 pb-[1px] uppercase line-clamp-1  ">{e.title}</div>
+        <div className=" text-sm lg:text-xl flex px-1  uppercase line-clamp-1  ">{e.title}</div>
+        <div className=" px-1 text-xs">₹{e.price * 6}</div>
       </Link>
-      <div className=" px-1 text-xs">₹{e.price * 6}</div>
     </div>
   );
 }
