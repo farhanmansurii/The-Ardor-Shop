565008d1f3a4806eecfb464f2e026cfe4883440c
Farhan <mansurifarhanfm@gmail.com>
2022-12-06 13:23:17 +0530

    s

--- components/Product.jsx
+++ components/Product.jsx
@@ -10,11 +10,11 @@ export default function Product({ e }) {
     dispatch(add(product));
   };
   return (
-    <div key={e.id} className="flex flex-col pb-2 hover:bg-secondary/20 border-primary font-light  mx-auto font-ibm  duration-200">
+    <div key={e.id} className="flex flex-col pb-2 max-h-[220px] lg:max-h-[300px] hover:bg-secondary/30 ease-linear  h border-primary font-light   mx-auto font-ibm  duration-200">
       <Link href={`/${e.id}`}>
           <div className=" absolute  flex flex-auto  place-content-end text-black p-2 place-self-end ">
             <BiHeart/></div>
-        <Image width={300} height={200} src={e.images[0]} alt={e.title} className=" mb-2 "/>
+        <Image width={300} height={200} src={e.images[0]} alt='' className=" mb-2 "/>
         <div className=" text-sm lg:text-xl flex px-1  mt-2   line-clamp-1  w-11/12 ">{e.title}</div>
         <div className=" px-1  my-2 text-sm font-light ">₹{e.price * 6}</div>
       </Link>
